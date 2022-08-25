108859e41de541761d7891bf2c40d45847f6421b
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-25 17:35:23 +0900

    'renew'

--- mod.js
+++ mod.js
@@ -1,4 +1,9 @@
-<<<<<<< HEAD
-javascirpt:try{setAndroidMainUUID('"+UUID+"');setPopupMessage('"+str+"');}catch(e){console.log(e)}
-=======
->>>>>>> 3f20d0c72210f5eb4f5a89d5f221e64f769e4c72
+function mneCall(date, callback) {
+  const els = doc.gcn("bookyesbgcolor");
+  Array.from(els).forEach((el) => {
+    const param = el.attr("onclick").inparen();
+    const fulldate = param[0];
+    dates.push([fulldate, param]);
+  });
+  callback();
+}
