7f45c94d842537b5cd4f3902281b9046c904eebf
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-25 17:35:39 +0900

    'renew'

--- mod.js
+++ mod.js
@@ -2,7 +2,7 @@ function mneCall(date, callback) {
   const els = doc.gcn("bookyesbgcolor");
   Array.from(els).forEach((el) => {
     const param = el.attr("onclick").inparen();
-    const fulldate = param[0];
+    const fulldate = param[0 ];
     dates.push([fulldate, param]);
   });
   callback();
