2c02d5d8f25abe49416227f9416bf7da70639f48
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-26 22:05:50 +0900

    'renew'

--- mod.js
+++ mod.js
@@ -1,9 +1,20 @@
 function mneCall(date, callback) {
-  const els = doc.gcn("bookyesbgcolor");
-  Array.from(els).forEach((el) => {
-    const param = el.attr("onclick").inparen();
-    const fulldate = param[0 ];
-    dates.push([fulldate, param]);
-  });
-  callback();
+  timer(2000, () => {
+    const param = {
+      V_IN_GOLF_ID: "S1",
+      V_IN_YEAR: date.gh(4),
+      V_IN_MONTH: date.gt(2),
+      V_IN_MEMNO: "",
+      T_MONTH: date.gt(2),
+    };
+    post("/golf.calendar.pns?getCalendar_S1", param, {}, (data) => {
+      const els = JSON.parse(data).entitys;
+      Array.from(els).forEach((el) => {
+        if (el.IT_OPEN != "신청하기") return;
+        const fulldate = el.ORI_CD_DATE.split("-").join("");
+        dates.push([fulldate, 0]);
+      });
+      callback();
+    });
+  });  
 }
