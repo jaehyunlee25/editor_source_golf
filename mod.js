116ffbb7162c29940a749136886846091e3defe7
jaehyunlee <jaehyunlee25@gmail.com>
2022-09-01 06:14:38 +0900

    'renew'

--- mod.js
+++ mod.js
@@ -1,4 +1,19 @@
-<<<<<<< HEAD
-javascirpt:try{setAndroidMainUUID('"+UUID+"');setPopupMessage('"+str+"');}catch(e){console.log(e)}
-=======
->>>>>>> 3f20d0c72210f5eb4f5a89d5f221e64f769e4c72
+function mneCall(date, callback) {
+  log("mneCall");
+
+  const param = {
+    V_IN_GOLF_ID: "P1",
+    V_IN_YEAR: date.gh(4),
+    V_IN_MONTH: date.gt(2),
+    V_IN_COURSE: "F",
+  };
+  post("/golf.calendar.pns?getCalendar", param, {}, (data) => {
+    const els = JSON.parse(data).entitys;
+    Array.from(els).forEach((el) => {
+      if (el.IT_OPEN_DESC != "신청하기") return;
+      const fulldate = el.ORI_CD_DATE.split("-").join("");
+      dates.push([fulldate, 0]);
+    });
+    callback();
+  });
+}
