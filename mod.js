445146e19c3e9f3a11358e1d57128fab184a8c13
jaehyunlee <jaehyunlee25@gmail.com>
2022-09-02 20:45:54 +0900

    'renew'

--- mod.js
+++ mod.js
@@ -1,20 +1,44 @@
 function mneCall(date, callback) {
-  timer(2000, () => {
+  timer(1000, () => { 
     const param = {
-      V_IN_GOLF_ID: "S1",
-      V_IN_YEAR: date.gh(4),
-      V_IN_MONTH: date.gt(2),
-      V_IN_MEMNO: "",
-      T_MONTH: date.gt(2),
+      clickTdId: "",
+      clickTdClass: "",
+      workMonth: date,
+      workDate: date + "01",
+      bookgDate: "",
+      bookgTime: "",
+      bookgCourse: "",
+      searchTime: "",
+      selfTYn: "",
+      macroChk: "",
+      temp001: "",
+      bookgComment: "",
+      memberCd: 61,
     };
-    post("/golf.calendar.pns?getCalendar_S1", param, {}, (data) => {
-      const els = JSON.parse(data).entitys;
-      Array.from(els).forEach((el) => {
-        if (el.IT_OPEN != "신청하기") return;
-        const fulldate = el.ORI_CD_DATE.split("-").join("");
-        dates.push([fulldate, 0]);
+    post("/reservation/ajax/golfCalendar", param, {}, (data) => {
+      const ifr = document.createElement("div");
+      ifr.innerHTML = data;
+  
+      const tbls = ifr.getElementsByClassName("cm_calender_tbl");
+      let as = [];
+      Array.from(tbls).forEach((tbl) => {
+        const arr = Array.from(tbl.getElementsByTagName("a"));
+        as = as.concat(arr);
       });
+  
+      as.forEach((a) => {
+        if (a.className === "cal_end") return;
+        const str = a.attr("onclick");
+        if (str.indexOf("CLOSE") !== -1) return;
+        if (str.indexOf("NOOPEN") !== -1) return;
+        console.log(str);
+        const [ , , date] = str.inparen();
+        log(date);
+        dates.push([date, 0]);
+      });
+  
       callback();
     });
-  });  
+
+  });
 }
