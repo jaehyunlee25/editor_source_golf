f42222940bd97ecd59f4f69c4355a4dfda773432
jaehyunlee <jaehyunlee25@gmail.com>
2022-12-12 14:42:44 +0900

    'renew'

--- js/regGolfClub.js
+++ js/regGolfClub.js
@@ -54,7 +54,7 @@ function trclick() {
 function setDetail() {
   const { back, content, close } = layerpop();
   const div = content.add("div");
-  div.css("margin:auto;width: 100%;");
+  div.css("margin:auto;width: 80%;");
   const tmplt = doc.querySelector("#tplDetail");
   const dtl = doc.importNode(tmplt.content, true);
   div.appendChild(dtl);
