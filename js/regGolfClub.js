b8f8801af6e88bf547eccbe00e24ce0f6f09d96e
jaehyunlee <jaehyunlee25@gmail.com>
2022-12-12 12:31:14 +0900

    'renew'

--- js/regGolfClub.js
+++ js/regGolfClub.js
@@ -1,6 +1,6 @@
 const httpHeader = { "Content-Type": "application/json" };
 const urlHeader = "https://dev.mnemosyne.co.kr/api/reservation/";
-const apiHeader = "https://dev.mnemosyne.co.kr/crawler/";
+const apiHeader = "https://dev.mnemosyne.co.kr/api/crawler/";
 const cf = new jCommon();
 
 main();
