8f51729e95bffd0408d4b35856d6a9c5893ea32c
jaehyunlee <jaehyunlee25@gmail.com>
2022-12-13 02:36:10 +0900

    'renew'

--- js/regGolfClub.js
+++ js/regGolfClub.js
@@ -16,7 +16,7 @@ function main() {
       return res;
     })(golfClubs);
     clubs = clubs.sort((a, b) => {
-      return new Date(a.updated_at) - new Date(b.updated_at);
+      return new Date(b.updated_at) - new Date(a.updated_at);
     });
     setList();
   });
