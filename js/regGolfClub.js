81d66b620a1e2dedb80eb04860fbaf6afc1b00b2
jaehyunlee <jaehyunlee25@gmail.com>
2022-12-16 15:24:11 +0900

    'renew'

--- js/regGolfClub.js
+++ js/regGolfClub.js
@@ -125,7 +125,7 @@ function setGroup() {
   const tabs = div.add("div");
   const names = div.add("div");
   const foot = div.add("div");
-  tabs.css("border-bottom:1px solid gray;");
+  tabs.css("border-bottom:1px solid gray;padding-bottom: 5px;");
   foot.css("text-align:center;");
   names.css("font-size: 20px;min-height: 400px;");
   Object.keys(groups).forEach((group) => {
