1b9eb8e9fa7ad8a0428304d8729945e659a5faa6
jaehyunlee <jaehyunlee25@gmail.com>
2023-08-18 18:50:48 +0900

    urlHeader_serverHeader_add

--- js/scriptregprocess.js
+++ js/scriptregprocess.js
@@ -1,21 +1,40 @@
 const httpHeader = { "Content-Type": "application/json" };
-const urlHeader = "https://mnemosynesolutions.co.kr/job";
+/* const urlHeader = "https://mnemosynesolutions.co.kr/job";
 const serverHeader = "https://dev.mnemosyne.co.kr/api/crawler";
-/* const urlHeader = "http://localhost:8038";
+const urlHeader = "http://localhost:8038";
 const serverHeader = "http://localhost:8080"; */
 const cf = new jCommon();
 let letters = [];
 let vertices;
+const lsObj = {
+  UUID: iptUUID,
+  engId: iptEngId,
+  urlHeader: selUrlHeader,
+  serverHeader: selServerHeader,
+};
+
+Object.entries(lsObj).forEach(([key, el], i) => {
+  el.value = localStorage.getItem(key);
+});
 
 main();
+btnSearch.onclick = main;
 function main() {
   getClubDataCheck();
   getServerDataCheck();
 }
+selUrlHeader.onchange = function () {
+  localStorage.setItem("urlHeader", this.value);
+};
+selServerHeader.onchange = function () {
+  localStorage.setItem("serverHeader", this.value);
+};
 function getClubDataCheck() {
+  if (iptUUID.value == "") return;
+  localStorage.setItem("UUID", iptUUID.value);
   post(
-    urlHeader + "/getClubDataCheck",
-    { clubId: "a43a52c5-707a-11ed-9c7a-0242ac110007" },
+    selUrlHeader.value + "/getClubDataCheck",
+    { clubId: iptUUID.value },
     httpHeader,
     (resp) => {
       const json = JSON.parse(resp);
@@ -24,17 +43,33 @@ function getClubDataCheck() {
   );
 }
 function getServerDataCheck() {
+  if (iptEngId.value == "") return;
+  localStorage.setItem("engId", iptEngId.value);
   post(
-    serverHeader + "/getServerDataCheck",
-    { engId: "kwangju" },
+    selServerHeader.value + "/getServerDataCheck",
+    { engId: iptEngId.value },
     httpHeader,
     (resp) => {
       const json = JSON.parse(resp);
-      log(json);
+      mkServerFileList(json);
     }
   );
 }
+function mkServerFileList(json) {
+  tblServerList.str("");
+  Object.entries(json).forEach(([key, val], i) => {
+    const tr = tblServerList.add("tr");
+    if (val == 0) tr.className = "notr";
+    const tdName = tr.add("td");
+    const tdCheck = tr.add("td");
+    tdName.className = "name";
+    tdCheck.className = "check";
+    tdName.str(key);
+    tdCheck.str(val);
+  });
+}
 function mkList(json) {
+  tblList.str("");
   Object.entries(json).forEach(([key, val], i) => {
     const tr = tblList.add("tr");
     if (val == 0) tr.className = "notr";
