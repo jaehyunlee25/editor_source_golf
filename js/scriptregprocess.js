9cd17cb13b2a4a3e79d746357413a4912988dcb2
jaehyunlee <jaehyunlee25@gmail.com>
2023-08-25 10:23:04 +0900

    clubdetail_edit

--- js/scriptregprocess.js
+++ js/scriptregprocess.js
@@ -19,9 +19,15 @@ Object.entries(lsObj).forEach(([key, el], i) => {
 
 main();
 btnSearch.onclick = main;
+selMembership.onchange = detailchange;
+selGrade.onchange = detailchange;
+selPrestige.onchange = detailchange;
+selLoginWall.onchange = detailchange;
+
 function main() {
   getClubDataCheck();
   getServerDataCheck();
+  getClubDetail();
 }
 selUrlHeader.onchange = function () {
   localStorage.setItem("urlHeader", this.value);
@@ -29,6 +35,38 @@ selUrlHeader.onchange = function () {
 selServerHeader.onchange = function () {
   localStorage.setItem("serverHeader", this.value);
 };
+function detailchange() {
+  if (iptUUID.value == "") return;
+  post(
+    selServerHeader.value + "/modGolfClubDetail",
+    {
+      golfClubId: iptUUID.value,
+      isMembership: selMembership.value * 1,
+      grade: selGrade.value * 1,
+      isPrestige: selPrestige.value * 1,
+      isLoginWall: selLoginWall.value * 1,
+    },
+    httpHeader,
+    (resp) => {
+      const { type, data } = JSON.parse(resp);
+      log(type);
+    }
+  );
+}
+function getClubDetail() {
+  if (iptUUID.value == "") return;
+  post(
+    selServerHeader.value + "/getGolfClubDetail",
+    { golfClubId: iptUUID.value },
+    httpHeader,
+    (resp) => {
+      const { type, data } = JSON.parse(resp);
+      if (type == "error") return;
+      const [json] = data;
+      mkClubDetail(json);
+    }
+  );
+}
 function getClubDataCheck() {
   if (iptUUID.value == "") return;
   localStorage.setItem("UUID", iptUUID.value);
@@ -54,6 +92,24 @@ function getServerDataCheck() {
       mkServerFileList(json);
     }
   );
+}
+function mkClubDetail(json) {
+  if (json.isMemberShip == 0) {
+    selMembership.value = 0;
+  } else {
+    selMembership.value = 1;
+  }
+  selGrade.value = json.grade + "";
+  if (json.isPrestige == 0) {
+    selPrestige.value = 0;
+  } else {
+    selPrestige.value = 1;
+  }
+  if (json.isLoginWall == 0) {
+    selLoginWall.value = 0;
+  } else {
+    selLoginWall.value = 1;
+  }
 }
 function mkServerFileList(json) {
   tblServerList.str("");
