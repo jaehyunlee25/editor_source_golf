a166bc92d385fc90699d843e0443c50b4a8c0a9e
jae hyun lee <jaehyunlee25@gmail.com>
2023-09-22 23:06:55 +0900

    monitor_add

---
+++ js/monitor.js
@@ -0,0 +1,56 @@
+const httpHeader = { "Content-Type": "application/json" };
+let monitorHeader = "https://dev.mnemosyne.co.kr/monitor";
+const cf = new jCommon();
+const log = console.log;
+
+get(".env", {}, httpHeader, (resp) => {
+  let { monitorHeader: url } = resp.jp();
+  monitorHeader = url;
+  main();
+});
+
+function main() {
+  const clubId = "126fd385-ee24-11ec-a93e-0242ac11000a";
+  conHomepage(clubId, (json) => {
+    log(json);
+    conLogin(clubId, (json) => {
+      log(json);
+      conSearch(clubId, (json) => {
+        log(json);
+      });
+    });
+  });
+}
+function conHomepage(clubId, callback) {
+  post(
+    monitorHeader + "/connect",
+    { clubId, type: "main" },
+    httpHeader,
+    (resp) => {
+      const json = resp.jp();
+      if (callback) callback(json);
+    }
+  );
+}
+function conLogin(clubId, callback) {
+  post(
+    monitorHeader + "/connect",
+    { clubId, type: "login" },
+    httpHeader,
+    (resp) => {
+      const json = resp.jp();
+      if (callback) callback(json);
+    }
+  );
+}
+function conSearch(clubId, callback) {
+  post(
+    monitorHeader + "/connect",
+    { clubId, type: "search" },
+    httpHeader,
+    (resp) => {
+      const json = resp.jp();
+      if (callback) callback(json);
+    }
+  );
+}
