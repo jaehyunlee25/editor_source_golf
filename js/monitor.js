d2a4660402ce8a2ac7242761e1c252137c42cf5a
jae hyun lee <jaehyunlee25@gmail.com>
2023-09-25 11:22:46 +0900

    history_add

--- js/monitor.js
+++ js/monitor.js
@@ -4,34 +4,16 @@ const cf = new jCommon();
 const log = console.log;
 let clublist;
 let mapClublist;
-
-get(".env", {}, httpHeader, (resp) => {
-  let { monitorHeader: url } = resp.jp();
-  monitorHeader = url;
-  main();
-});
-
-async function main() {
-  clublist = await getGolfClubList();
-  mapClublist = clublist.getmap("id");
-  // log(clublist);
-  return;
-  const clubId = "126fd385-ee24-11ec-a93e-0242ac11000a";
-
-  conHomepage(clubId, (json) => {
-    log(json);
-    conLogin(clubId, (json) => {
-      log(json);
-      conSearch(clubId, (json) => {
-        log(json);
-      });
-    });
-  });
-}
-function getGolfClubList() {
+let FLAG = true;
+let timer;
+let keyStack = [];
+let history;
+let mapHistory = {};
+String.prototype.api = function () {
+  const api = this.toString();
   const prom = new Promise((res, rej) => {
     try {
-      post(monitorHeader + "/getGolfClubList", {}, httpHeader, (resp) => {
+      post(monitorHeader + "/" + api, {}, httpHeader, (resp) => {
         const json = resp.jp();
         res(json);
       });
@@ -40,39 +22,127 @@ function getGolfClubList() {
     }
   });
   return prom;
+};
+
+get(".env", {}, httpHeader, (resp) => {
+  let { monitorHeader: url } = resp.jp();
+  monitorHeader = url;
+  main();
+});
+
+async function main() {
+  clublist = await "getGolfClubList".api();
+  mapClublist = clublist.getmap("id");
+  history = await "getHistory".api();
+  history.forEach((obj) => {
+    const { clubId, engId, name, type, average_diff } = obj;
+    mapHistory[clubId] ??= { engId, name, main: -1, login: -1, search: -1 };
+    mapHistory[clubId][type] = average_diff;
+  });
+  mkHistory();
 }
-function conHomepage(clubId, callback) {
-  post(
-    monitorHeader + "/connect",
-    { clubId, type: "main" },
-    httpHeader,
-    (resp) => {
-      const json = resp.jp();
-      if (callback) callback(json);
+function mkHistory() {
+  log(mapHistory);
+  const table = elHistory.add("table");
+  table.border = 1;
+  const thead = table.add("thead");
+  const htr1 = thead.add("tr");
+  const htr2 = thead.add("tr");
+  ["클럽아이디", "클럽영문", "클럽이름"].forEach((str) => {
+    const htd = htr1.add("td");
+    htd.rowSpan = 2;
+    htd.str(str);
+    htd.className = "head";
+  });
+  const htd1 = htr1.add("td");
+  htd1.colSpan = 3;
+  htd1.str("반응속도(ms)");
+  htd1.className = "head";
+  ["main", "login", "search"].forEach((str) => {
+    const htd = htr2.add("td");
+    htd.str(str);
+    htd.className = "head";
+  });
+
+  const tbody = table.add("tbody");
+  Object.entries(mapHistory).forEach(
+    ([clubId, { engId, name, main, login, search }]) => {
+      const tr = tbody.add("tr");
+      [clubId, engId, name, main, login, search].forEach((val, i) => {
+        const td = tr.add("td");
+        td.str(val);
+        if (i > 2) td.style.textAlign = "right";
+        if (val == -1) td.style.backgroundColor = "pink";
+      });
     }
   );
+}
+function conHomepage(clubId) {
+  return new Promise((res, rej) => {
+    post(
+      monitorHeader + "/connect",
+      { clubId, type: "main" },
+      httpHeader,
+      (resp) => {
+        const json = resp.jp();
+        res(json);
+      }
+    );
+  });
 }
 function conLogin(clubId, callback) {
-  post(
-    monitorHeader + "/connect",
-    { clubId, type: "login" },
-    httpHeader,
-    (resp) => {
-      const json = resp.jp();
-      if (callback) callback(json);
-    }
-  );
+  return new Promise((res, rej) => {
+    post(
+      monitorHeader + "/connect",
+      { clubId, type: "login" },
+      httpHeader,
+      (resp) => {
+        const json = resp.jp();
+        res(json);
+      }
+    );
+  });
 }
 function conSearch(clubId, callback) {
-  post(
-    monitorHeader + "/connect",
-    { clubId, type: "search" },
-    httpHeader,
-    (resp) => {
-      const json = resp.jp();
-      if (callback) callback(json);
-    }
-  );
+  return new Promise((res, rej) => {
+    post(
+      monitorHeader + "/connect",
+      { clubId, type: "search" },
+      httpHeader,
+      (resp) => {
+        const json = resp.jp();
+        res(json);
+      }
+    );
+  });
+}
+function clubselect(clubId) {
+  iptClubSearch.value = "";
+  iptClubSearch.onkeyup();
+  const club = mapClublist[clubId];
+  elSelectedClub.str(club.name);
+  elSelectedClub.club = club;
+  elDesc.str(JSON.stringify(club, null, 4));
+}
+function consecutive(clubId) {
+  conHomepage(clubId, async (json) => {
+    elResult.str(JSON.stringify(json, null, 4));
+    await delay(1000);
+    conLogin(clubId, async (json) => {
+      elResult.str(JSON.stringify(json, null, 4));
+      await delay(1000);
+      conSearch(clubId, async (json) => {
+        elResult.str(JSON.stringify(json, null, 4));
+        await delay(1000);
+        FLAG = true;
+      });
+    });
+  });
+}
+function delay(milliseconds) {
+  return new Promise((res, rej) => {
+    setTimeout(res, milliseconds); // return 할 게 없으니, 따로 reject을 설정하지 않는다.
+  });
 }
 iptClubSearch.onkeyup = function () {
   elSelect.str("");
@@ -94,35 +164,23 @@ iptClubSearch.onkeyup = function () {
     a.club = club;
   });
 };
-function clubselect(clubId) {
-  iptClubSearch.value = "";
-  iptClubSearch.onkeyup();
-  const club = mapClublist[clubId];
-  log(club);
-  elSelectedClub.str(club.name);
-  elSelectedClub.club = club;
-  elDesc.str(JSON.stringify(club, null, 4));
-}
-btnConHomepage.onclick = function () {
+btnConHomepage.onclick = async function () {
   elResult.str("");
   const { id } = elSelectedClub.club;
-  conHomepage(id, (resp) => {
-    elResult.str(JSON.stringify(resp, null, 4));
-  });
+  const resp = await conHomepage(id);
+  elResult.str(JSON.stringify(resp, null, 4));
 };
-btnConLoginpage.onclick = function () {
+btnConLoginpage.onclick = async function () {
   elResult.str("");
   const { id } = elSelectedClub.club;
-  conLogin(id, (resp) => {
-    elResult.str(JSON.stringify(resp, null, 4));
-  });
+  const resp = await conLogin(id);
+  elResult.str(JSON.stringify(resp, null, 4));
 };
-btnConSearchpage.onclick = function () {
+btnConSearchpage.onclick = async function () {
   elResult.str("");
   const { id } = elSelectedClub.club;
-  conSearch(id, (resp) => {
-    elResult.str(JSON.stringify(resp, null, 4));
-  });
+  const resp = await conSearch(id);
+  elResult.str(JSON.stringify(resp, null, 4));
 };
 btnExecLogin.onclick = function () {
   elResult.str("");
@@ -131,4 +189,52 @@ btnExecLogin.onclick = function () {
 btnExecDateSearch.onclick = function () {
   elResult.str("");
   alert("구현중입니다.");
+};
+allStart.onclick = function () {
+  elProgress.str("전체 골프장 연결테스트를 진행중입니다...");
+  if (this.str() == "시작") {
+    keyStack = Object.keys(mapClublist);
+    this.str("계속");
+  } else {
+    this.str("시작");
+  }
+  let timercount = 0;
+  timer = setInterval(async () => {
+    timercount++;
+    log(timercount);
+    if (timercount > 20) {
+      timercount = 0;
+      FLAG = true;
+      log("timeout");
+      return;
+    }
+    if (!FLAG) {
+      return;
+    }
+    FLAG = false;
+    const clubId = keyStack.shift();
+    if (!clubId) {
+      log("the end of all work!");
+      clearInterval(timer);
+      return;
+    }
+    log(clubId);
+    clubselect(clubId);
+    const resHomepage = await conHomepage(clubId);
+    elResult.str(JSON.stringify(resHomepage, null, 4));
+    timercount = 0;
+    // await delay(5000);
+    const resLogin = await conLogin(clubId);
+    elResult.str(JSON.stringify(resLogin, null, 4));
+    timercount = 0;
+    // await delay(5000);
+    const resSearch = await conSearch(clubId);
+    elResult.str(JSON.stringify(resSearch, null, 4));
+    // await delay(5000);
+    FLAG = true;
+    timercount = 0;
+  }, 1000);
+};
+allStop.onclick = function () {
+  clearInterval(timer);
 };
