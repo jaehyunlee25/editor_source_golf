bfbe60a5aef2821100312f433606c25b2073798a
jae hyun lee <jaehyunlee25@gmail.com>
2023-10-26 10:06:30 +0900

    'deploy_168'

--- exec.bat
+++ exec.bat
@@ -1,4 +1,4 @@
 git add -A
-git commit -m 'deploy_165'
+git commit -m 'deploy_168'
 git push origin main
 ssh root@mnemosynesolutions.co.kr -t "cd /var/www/html/app/project/editor_source_golf; git pull origin main"
